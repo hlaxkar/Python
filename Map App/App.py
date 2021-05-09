909ab44c509cc2107ad90d4147912169d7f309d0
Harshit laxkar <hlaxkar@gmail.com>
2021-05-09 19:21:15 +0530

    update

--- Map App/App.py
+++ Map App/App.py
@@ -19,13 +19,14 @@ html = """ Name: <br>
 <a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
 Height: %s m
 """
-map = folium.Map(location = [38,97],zoom_start=6, tiles= "http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}", attr="https://www.google.com")
+map = folium.Map(location = [36.09,-116.71],zoom_start=5, tiles= "http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}", attr="https://www.google.com")
 
 fg = folium.FeatureGroup(name="my map")
 for lt,ln,nm,con,ele in zip(lat,lon,name,cont,elev):
     if (con == "United States"):
 
-        folium.IFrame(html=html % (name, name, ele), width=200, height=100)
-        fg.add_child(folium.Marker(location=[lt,ln], popup=nm, icon=folium.Icon(color=color_picker(ele)), tooltip=nm,  ))
+        iframe= folium.IFrame(html=html % (nm, nm, ele), width=200, height=100)
+        fg.add_child(folium.CircleMarker(location=[lt,ln], popup=folium.Popup(iframe), 
+        fill_color=color_picker(ele), tooltip=nm,radius=7, color='grey', fill_opacity=0.7 ))
 map.add_child(fg)
 map.save("map1.html")
