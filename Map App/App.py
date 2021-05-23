de9b5e5ab8bb59da0292c98ff742ca2d73949514
Harshit laxkar <hlaxkar@gmail.com>
2021-05-23 21:58:18 +0530

    update

--- Map App/App.py
+++ Map App/App.py
@@ -21,12 +21,19 @@ Height: %s m
 """
 map = folium.Map(location = [36.09,-116.71],zoom_start=5, tiles= "http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}", attr="https://www.google.com")
 
-fg = folium.FeatureGroup(name="my map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 for lt,ln,nm,con,ele in zip(lat,lon,name,cont,elev):
     if (con == "United States"):
 
         iframe= folium.IFrame(html=html % (nm, nm, ele), width=200, height=100)
-        fg.add_child(folium.CircleMarker(location=[lt,ln], popup=folium.Popup(iframe), 
+        fgv.add_child(folium.CircleMarker(location=[lt,ln], popup=folium.Popup(iframe), 
         fill_color=color_picker(ele), tooltip=nm,radius=7, color='grey', fill_opacity=0.7 ))
-map.add_child(fg)
+
+fgp = folium.FeatureGroup(name="Population")
+fgp.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig').read(), 
+                            style_function=lambda x: {'fillColor':'yellow' if x['properties']['POP2005']<10000000 else 'orange' if 10000000<= x['properties']['POP2005'] <20000000 else 'red' })  )
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 map.save("map1.html")
